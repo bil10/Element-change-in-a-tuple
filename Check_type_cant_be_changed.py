67f9a9a210850c56285c3162c3b7ecb43e3db359
bil10 <66562471+bil10@users.noreply.github.com>
2021-12-29 15:31:26 +0200

    Add files via upload

---
+++ Check_type_cant_be_changed.py
@@ -0,0 +1,5 @@
+# to check whether the element in a tuple can be changed or not
+
+a = (1,2,3,4,5,6,7,8)
+a[0] = 32
+print(a) # object means tuple type of elements inside a tuple or list or anything etc
